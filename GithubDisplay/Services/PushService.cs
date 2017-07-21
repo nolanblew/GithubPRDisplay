c56cf8916db3e270a9db9388d54760de9c549a75
Nolan Blew <nolanblew@hotmail.com>
2017-07-20 18:10:51 -0700

    Add toast service and test

---
+++ GithubDisplay/Services/PushService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using Windows.UI.Notifications;
+
+namespace GithubDisplay.Services
+{
+    public static class PushService
+    {
+        public static void SendPush(string title, string message, string URL = null)
+        {
+            if (App.RunningOnXbox) { return; }
+
+            ToastContent content = new ToastContent()
+            {
+                Visual = new ToastVisual
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                        {
+                            new AdaptiveText()
+                            {
+                                Text = title,
+                                HintMaxLines = 1
+                            },
+
+                            new AdaptiveText()
+                            {
+                                Text = message
+                            }
+                        }
+                    }
+                }
+            };
+            if (!string.IsNullOrWhiteSpace(URL)) { content.Launch = $"pr:{URL}"; }
+
+            var toast = new ToastNotification(content.GetXml());
+            toast.ExpirationTime = DateTime.Now.AddDays(1);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
